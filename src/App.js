4562e5b8d77313a1c0639dd65ee3c4178d979ab8
Arun <arun.viswanath@yahoo.com>
2023-01-21 19:18:02 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Update  <code>src/App.js</code> a for changes Arun abcdefgh.
+          i am doing great
+        </p>
+        <p>
+          Hello world how are you doing?
+          
+        </p>
+        <p>
+          Funtastic
         </p>
         <a
           className="App-link"
